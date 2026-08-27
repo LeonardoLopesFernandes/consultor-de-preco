72548231be7848a35095ebbec54a19f7ffd03dec
ConsultorPreco <dev@consultorpreco.local>
2026-08-27 06:45:21 +0000

    fix(android): forca compileSdk 36 em todos os modulos (corrige checkAarMetadata do share_plus) e desativa newDsl do AGP 9

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -7,6 +7,23 @@ allprojects {
 
 subprojects {
     project.evaluationDependsOn(":app")
+    if (state.executed) {
+        bumpCompileSdk()
+    } else {
+        afterEvaluate { bumpCompileSdk() }
+    }
+}
+
+fun Project.bumpCompileSdk() {
+    val ext = extensions.findByName("android") ?: return
+    try {
+        ext.javaClass.getMethod("setCompileSdk", Integer.TYPE).invoke(ext, 36)
+    } catch (_: NoSuchMethodException) {
+        try {
+            ext.javaClass.getMethod("setCompileSdkVersion", Integer.TYPE).invoke(ext, 36)
+        } catch (_: Throwable) {
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
